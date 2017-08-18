d7788c6b83f59c458704a403dd811ce9b36328e5
Salvador <salvador.og@gmail.com>
2017-08-18 05:12:20 -0300

    Fixed typo

--- app/controllers/body_masses_controller.rb
+++ app/controllers/body_masses_controller.rb
@@ -3,17 +3,13 @@ class BodyMassesController < ApplicationController
   before_action :calculate_body_mass_index, only: [:create]
 
   def create
-    json_response(@bmi_report)
+    json_response(@bmi)
   end
 
   private
 
-  def body_mass_params
-    params.permit(:height, :weight)
-  end
-
   def calculate_body_mass_index
-    @service = BodyMassService.new(body_mass_params)
-    @bmi_report = @service.report
+    @service = BodyMassService.new(params[:weight], params[:height])
+    @bmi = @service.report
   end
 end
